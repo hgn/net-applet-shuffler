52e518c686adf0d3c6915a376b19a7a28123544a
Hagen Paul Pfeifer <hagen@jauu.net>
2016-02-16 20:26:48 +0100

    add more comments to example campaign

--- campaigns/001-iw10-std/run.cmd
+++ campaigns/001-iw10-std/run.cmd
@@ -16,14 +16,17 @@ exec 003-restore-sysctls beta
 exec 003-restore-sysctls koppa
 
 # start netperf as receiver at TCP port
-# 8000 and background process
-exec xxx-netperf-sink beta 8000
+# 8000 and background process. This applet
+# probably copy a local python helper program
+# to node beta.
+# xxx- prefixed applets are not implemented yet
+exec xxx-netperf-sink beta mode:background port:8000
 
 # limit rate at forwarding radio to 10000 byte/sec
 exec xxx-netem koppa rate:10000
 
 # set IW at sender to 20
-exec xxx-set-iw alpha 20
+exec xxx-conf-iw alpha 20
 # send 10000 byte from to beta (at node alpha)
 # do not background, wait in foreground, process
 # will block here until all bytes are transmitted
@@ -36,6 +39,7 @@ exec xxx-netpert-sender alpha beta 10000
 # simple return the data writen with the previous backgrounded
 # "exec xxx-netperf-sink beta 8000" call. The data file
 # is copied here, the file at beta is removed (cleaned)
-exec xxx-netperf-sink beta data
+exec xxx-netperf-sink beta stop-backgrouding
+exec xxx-netperf-sink beta fetch-data
 
 
