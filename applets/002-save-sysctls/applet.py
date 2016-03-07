4b00d17c27eb0376386d88673ed36ce2825ed00c
Daniel Metz <dmetz@mytum.de>
2016-03-07 08:38:11 +0100

    fixing backup path errors

--- applets/002-save-sysctls/applet.py
+++ applets/002-save-sysctls/applet.py
@@ -6,14 +6,14 @@ def main(x, conf, args):
         return False
 
     hostname = args[0]
-    x.p.msg("save sysctl to /etc/sysctl.save at host {}\n".format(hostname))
+    x.p.msg("save sysctl to /tmp/sysctl.save at host {}\n".format(hostname))
     x.p.msg("use restore to restore values")
 
     ip = conf['boxes'][hostname]["interfaces"][0]['ip-address']
     user = conf['boxes'][hostname]['user']
     # check if file exist already, if we break here
     # x.ssh.exec execute at ip with sudo
-    stdout, stderr, exit_code = x.ssh.execute(ip, user, "test -f /etc/sysctl.save")
+    stdout, stderr, exit_code = x.ssh.execute(ip, user, "test -f /tmp/sysctl.save")
     if exit_code != 0:
         x.p.msg("file already available, nothing to do here")
 		# return true because it is not failure
--- applets/003-restore-sysctls/applet.py
+++ applets/003-restore-sysctls/applet.py
@@ -5,14 +5,14 @@ def main(x, conf, args):
         return False
 
     hostname = args[0]
-    x.p.msg("restoring sysctl from /etc/sysctl.save at host {}\n".format(hostname))
+    x.p.msg("restoring sysctl from /tmp/sysctl.save at host {}\n".format(hostname))
 
     ip = conf['boxes'][hostname]["interfaces"][0]['ip-address']
     user = conf['boxes'][hostname]['user']
 
     # check if a backup sysctl.save file exists
     # x.ssh.exec execute at ip with sudo
-    stdout, stderr, exit_code = x.ssh.execute(ip, user, "test -f /etc/sysctl.save")
+    stdout, stderr, exit_code = x.ssh.execute(ip, user, "test -f /tmp/sysctl.save")
     if exit_code == 0:
         x.p.msg("there is no sysctl.save backup which could be restored")
 		# return true because it is not failure
