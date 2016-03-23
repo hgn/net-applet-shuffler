75db9ece0f75c63a2c17c6412d50cedd642e5b75
Daniel Metz <dmetz@mytum.de>
2016-03-23 14:23:14 +0100

    added important hint

--- campaigns/001-netperf-data-exchange/run.cmd
+++ campaigns/001-netperf-data-exchange/run.cmd
@@ -52,6 +52,8 @@ exec 006-tcpdump beta id:0001 mode:start local-file-name:"ignored" filter:"tcp a
 # - flow_length is in seconds, or if negative, in bytes
 # - flow_offset is the time in seconds after which the flow is started
 # - netserver is the control(!) port of the netserver
+# - IMPORTANT: due to command and distribution problems on congested networks,
+#               the flow offsets should include enough time for setup
 # usage: host sink:[name] id:[string] source_port:[port] sink_port:[port] flow_length:[seconds|-bytes] flow_offset:[seconds] netserver:[port]
 exec 005-netperf alpha sink:beta id:0001 source_port:20000 sink_port:30000 flow_length:20 flow_offset:5 netserver:29999
 exec 005-netperf beta sink:alpha id:0002 source_port:20001 sink_port:30001 flow_length:20 flow_offset:10 netserver:29998
@@ -67,7 +69,7 @@ sleep 4
 # - every host:id tuple actively participating in the test should be specified here
 # - one host can have several running ids, but one (unique) id can have only one host
 # usage: interval_time:[seconds] [name_1]:[id_1] [name_1]:[id_2] [name_2]:[id_3] ...
-exec 008-wait-for-completion interval_time:5 alpha:0001 beta:0002
+exec 008-wait-for-completion interval_time:1 alpha:0001 beta:0002
 
 
 # tcpdump
