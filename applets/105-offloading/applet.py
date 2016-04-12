27d3d25edd73f1be9aff35df468e7969a63ce6e7
Daniel Metz <dmetz@mytum.de>
2016-04-12 13:21:59 +0200

    removing blank lines, changing usage to offloading

--- applets/105-offloading/applet.py
+++ applets/105-offloading/applet.py
@@ -1,37 +1,41 @@
-# usage: exec [host] command:"[command]"
+# usage: exec [host] offloading:[on|off]
 #
 # Check success:
 #  - sudo ethtool -k [device_name]
 #
 # Examples:
 #  - sudo ethtool -K enp4s0 tso off ufo off gso off gro off lro off
-#  - exec-applet 105-offloading alpha command:"tso off ufo off gso off gro off
-#    lro off"
+#  - exec-applet 105-offloading alpha offloading:off
 #
 # Hints:
 #  - "Cannot change [...]" messages probably mean that the value can not be
 #    changed (is fixed)
+#  - ethtool feedback seems to be faulty, so even a seemingly error message can
+#    mean success
 
 
 def print_usage(x):
-
     x.p.msg("\n usage:\n", False)
-    x.p.msg(" - exec 105-offloading [host] command:\"[command]\"\n", False)
+    x.p.msg(" - exec 105-offloading [host] offloading:\"[on|off]\"\n", False)
     x.p.msg("\n check success:\n", False)
     x.p.msg(" - ethtool -k [interface]\n", False)
     x.p.msg("\nexample:\n", False)
-    x.p.msg(" - exec-applet 105-offloading alpha command:"
-            "\"tso off ufo off gso off gro off lro off\"\n",
-            False)
-
-    return False
+    x.p.msg(" - exec-applet 105-offloading alpha offloading:on\n", False)
 
 
 def set_offloading(x, conf, dic):
-
     device_list = conf.get_all_test_iface_names(dic["host"])
     for device in device_list:
-        cmd = "ethtool -K {} {}".format(device, dic["cmd"])
+        cmd = str()
+        if dic["offloading"] == "on":
+            cmd = "ethtool -K {} tso on ufo on gso on gro on lro on"\
+                .format(device)
+        elif dic["offloading"] == "off":
+            cmd = "ethtool -K {} tso off ufo off gso off gro off lro off"\
+                .format(device)
+        else:
+            x.p.err("error: wrong usage")
+            return False
         # unfortunately, error handling does not work, since ethtool sometimes
         # even exits with code 1 on success
         _, _, exit_code = x.ssh.exec(dic["ip_control"], dic["user"], cmd)
@@ -45,9 +49,9 @@ def set_offloading(x, conf, dic):
 
 
 def main(x, conf, args):
-
     if "?" in args:
-        return print_usage(x)
+        print_usage(x)
+        return False
     if not len(args) >= 2:
         x.p.err("error: wrong usage\n")
         return False
@@ -56,15 +60,7 @@ def main(x, conf, args):
     dic = dict()
     try:
         dic["host"] = args[0]
-        position = 1
-        command_str = args[1].split(":")[1]
-        try:
-            while True:
-                position += 1
-                command_str += " " + args[position]
-        except IndexError:
-            pass
-        dic["cmd"] = command_str.strip("\"")
+        dic["offloading"] = args[1].split(":")[1]
     except IndexError:
         x.p.err("error: wrong usage\n")
         return False
