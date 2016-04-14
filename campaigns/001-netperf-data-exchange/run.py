d445f1327d457976fc3a1dea1c9272c35571867a
Daniel Metz <dmetz@mytum.de>
2016-04-14 09:13:09 +0200

    minor fixes

--- campaigns/001-netperf-data-exchange/run.py
+++ campaigns/001-netperf-data-exchange/run.py
@@ -36,6 +36,7 @@ def main(x):
 
     # netem
     # add delay to left and right outgoing interfaces for the upcoming test
+    # restoration is covered by 007-kill
     x.exec('104-netem-cmd koppa control:part change:add to-network:red command:"delay 10ms"')
     x.exec('104-netem-cmd koppa control:part change:add to-network:blue command:"delay 10ms"')
 
@@ -82,8 +83,8 @@ def main(x):
     # - flow_offset is the time in seconds after which the flow is started
     # - netserver is the control(!) port of the netserver
     # usage: host sink:[name] id:[string] source_port:[port] sink_port:[port] flow_length:[seconds|-bytes] flow_offset:[seconds] netserver:[port]
-    x.exec('005-netperf alpha sink:beta id:0001 source_port:20000 sink_port:30000 flow_length:6 flow_offset:1 netserver:29999')
-    x.exec('005-netperf beta sink:alpha id:0002 source_port:20001 sink_port:30001 flow_length:6 flow_offset:3 netserver:29998')
+    x.exec('005-netperf alpha sink:beta id:0002 source_port:20000 sink_port:30000 flow_length:6 flow_offset:1 netserver:29999')
+    x.exec('005-netperf beta sink:alpha id:0003 source_port:20001 sink_port:30001 flow_length:6 flow_offset:3 netserver:29998')
 
     # sleep seconds
     time.sleep(2)
@@ -95,7 +96,7 @@ def main(x):
     # - every host:id tuple actively participating in the test should be specified here
     # - one host can have several running ids, but one (unique) id can have only one host
     # usage: interval_time:[seconds] [name_1]:[id_1] [name_1]:[id_2] [name_2]:[id_3] ...
-    x.exec('008-wait-for-completion interval_time:5 alpha:0001 beta:0002')
+    x.exec('008-wait-for-completion interval_time:5 alpha:0002 beta:0003')
 
     # tcpdump
     # stop tcpdump on host, which also collects the dumpfile
@@ -103,7 +104,7 @@ def main(x):
     # - mode:stop -> the filter is ignored
     # - id and host MUST match the id of the started tcpdump
     # usage: host id:[string] mode:[start|stop] local-file-name:"path_and_filename" filter:"tcpdump filter string"
-    x.exec('006-tcpdump beta id:0001 mode:stop local-file-name:"./dumps/001_netperf_alpha_to_beta.pcap"')
+    x.exec('006-tcpdump beta id:0001 mode:stop local-file-name:"./001-netperf-data-exchange/dumpfiles/dump.pcap"')
 
     # netem
     # manual cleanup, remove the added delays
